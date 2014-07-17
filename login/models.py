ea8efd61c9c4ff2e92248d36699ca0f175147a3c
Gabriel Souza Victorino Lopes <gabriel.s.v.lopes@gmail.com>
2014-07-17 11:15:15 -0300

    [login] Delete the website input on registration
    Signed off by: Heitor Reis <marcheing@gmail.com>
    Signed off by: Filipe Vaz <vazfilipe92@gmail.com>

--- login/models.py
+++ login/models.py
@@ -5,9 +5,6 @@ class UserProfile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
     user = models.OneToOneField(User)
 
-    # The additional attributes we wish to include.
-    website = models.URLField(blank=True)
-
     # Override the __unicode__() method to return out something meaningful!
     def __unicode__(self):
         return self.user.username
--- login/views.py
+++ login/views.py
@@ -1,4 +1,4 @@
-from login.forms import UserForm, UserProfileForm
+from login.forms import UserForm
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login
@@ -19,10 +19,9 @@ def register(request):
         # Attempt to grab information from the raw form information.
         # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
 
         # If the two forms are valid...
-        if user_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid():
             # Save the user's form data to the database.
             user = user_form.save()
 
@@ -31,15 +30,6 @@ def register(request):
             user.set_password(user.password)
             user.save()
 
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves, we set commit=False.
-            # This delays saving the model until we're ready to avoid integrity problems.
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            # Now we save the UserProfile model instance.
-            profile.save()
-
             # Update our variable to tell the template registration was successful.
             registered = True
 
@@ -47,18 +37,17 @@ def register(request):
         # Print problems to the terminal.
         # They'll also be shown to the user.
         else:
-            print user_form.errors, profile_form.errors
+            print user_form.errors
 
     # Not a HTTP POST, so we render our form using two ModelForm instances.
     # These forms will be blank, ready for user input.
     else:
         user_form = UserForm()
-        profile_form = UserProfileForm()
 
     # Render the template depending on the context.
     return render_to_response(
             'register.html',
-            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
+            {'user_form': user_form, 'registered': registered},
             context)
 
 def user_login(request):

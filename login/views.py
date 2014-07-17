22414be8db40fe2809fddae74ac72d51f5a6df7c
Gabriel Souza Victorino Lopes <gabriel.s.v.lopes@gmail.com>
2014-07-17 06:19:01 -0300

    [login] Insert login required decorator on logout
    Signed off by: Heitor Reis <marcheing@gmail.com>
    Signed off by: Filipe Vaz <vazfilipe92@gmail.com>

--- login/views.py
+++ login/views.py
@@ -104,7 +104,7 @@ def user_login(request):
 
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
-#@login_required
+@login_required
 def user_logout(request):
     # Since we know the user is logged in, we can now just log them out.
     logout(request)

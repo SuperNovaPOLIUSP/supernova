f31300417395dec275aa4793ab1a31af6aa0da49
Gabriel Souza Victorino Lopes <gabriel.s.v.lopes@gmail.com>
2014-07-23 10:08:44 -0300

    [interface_professor] Finish tests on professor page
    Was necessary to create a file models.py that represents the supernova
    database, a file routers.py that redirects the database operations and
    a newTestRunner.py that allows unmanaged models to be created on the
    test database.
    
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- interface/models.py
+++ interface/models.py
@@ -1,3 +1,351 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Remove `managed = False` lines if you wish to allow Django to create and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
+# into your database.
+
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+class Academicprogram(models.Model):
+    idacademicprogram = models.IntegerField(db_column='idAcademicProgram', primary_key=True) # Field name made lowercase.
+    name = models.CharField(max_length=255)
+    vacancynumber = models.IntegerField(db_column='vacancyNumber', blank=True, null=True) # Field name made lowercase.
+    abbreviation = models.CharField(max_length=45)
+    startdate = models.DateField(db_column='startDate') # Field name made lowercase.
+    enddate = models.DateField(db_column='endDate') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'academicProgram'
+
+class AggrOffer(models.Model):
+    idoffer = models.IntegerField(db_column='idOffer') # Field name made lowercase.
+    idcourse = models.ForeignKey('Course', db_column='idCourse') # Field name made lowercase.
+    idprofessor = models.ForeignKey('Professor', db_column='idProfessor') # Field name made lowercase.
+    idtimeperiod = models.ForeignKey('Timeperiod', db_column='idTimePeriod') # Field name made lowercase.
+    classnumber = models.IntegerField(db_column='classNumber') # Field name made lowercase.
+    practical = models.IntegerField()
+    numberofregistrations = models.IntegerField(db_column='numberOfRegistrations', blank=True, null=True) # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'aggr_offer'
+
+class AggrOpticalsheetfield(models.Model):
+    idopticalsheetfield = models.IntegerField(db_column='idOpticalSheetField', primary_key=True) # Field name made lowercase.
+    idopticalsheet = models.ForeignKey('Opticalsheet', db_column='idOpticalSheet') # Field name made lowercase.
+    idoffer = models.ForeignKey(AggrOffer, db_column='idOffer') # Field name made lowercase.
+    code = models.IntegerField(blank=True, null=True)
+    courseindex = models.IntegerField(db_column='courseIndex', blank=True, null=True) # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'aggr_opticalSheetField'
+
+class AggrSurvey(models.Model):
+    idsurvey = models.IntegerField(db_column='idSurvey') # Field name made lowercase.
+    idopticalsheet = models.ForeignKey('Opticalsheet', db_column='idOpticalSheet') # Field name made lowercase.
+    idquestionnaire = models.ForeignKey('Questionnaire', db_column='idQuestionnaire') # Field name made lowercase.
+    assessmentnumber = models.IntegerField(db_column='assessmentNumber') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'aggr_survey'
+
+class Alternativemeaning(models.Model):
+    idalternativemeaning = models.IntegerField(db_column='idAlternativeMeaning', primary_key=True) # Field name made lowercase.
+    idanswertype = models.ForeignKey('Answertype', db_column='idAnswerType') # Field name made lowercase.
+    alternative = models.CharField(max_length=1)
+    meaning = models.TextField(blank=True)
+    class Meta:
+        managed = False
+        db_table = 'alternativeMeaning'
+
+class Answer(models.Model):
+    idanswer = models.IntegerField(db_column='idAnswer', primary_key=True) # Field name made lowercase.
+    questionindex = models.IntegerField(db_column='questionIndex') # Field name made lowercase.
+    iddatafile = models.ForeignKey('Datafile', db_column='idDatafile') # Field name made lowercase.
+    alternative = models.CharField(max_length=1)
+    identifier = models.IntegerField()
+    class Meta:
+        managed = False
+        db_table = 'answer'
+
+class Answertype(models.Model):
+    idanswertype = models.IntegerField(db_column='idAnswerType', primary_key=True) # Field name made lowercase.
+    name = models.CharField(max_length=45)
+    class Meta:
+        managed = False
+        db_table = 'answerType'
+
+class Classrepresentative(models.Model):
+    idclassrepresentative = models.IntegerField(db_column='idClassRepresentative', primary_key=True) # Field name made lowercase.
+    name = models.CharField(max_length=255)
+    memberid = models.IntegerField(db_column='memberId') # Field name made lowercase.
+    email = models.CharField(max_length=255)
+    cellphonenumber = models.IntegerField(db_column='cellphoneNumber', blank=True, null=True) # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'classRepresentative'
+
+class Course(models.Model):
+    idcourse = models.IntegerField(db_column='idCourse', primary_key=True) # Field name made lowercase.
+    name = models.CharField(max_length=255)
+    abbreviation = models.CharField(max_length=255)
+    syllabus = models.TextField(blank=True)
+    coursecode = models.CharField(db_column='courseCode', max_length=7) # Field name made lowercase.
+    startdate = models.DateField(db_column='startDate') # Field name made lowercase.
+    enddate = models.DateField(db_column='endDate') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'course'
+
+class Coursecoordination(models.Model):
+    idcoursecoordination = models.IntegerField(db_column='idCourseCoordination', primary_key=True) # Field name made lowercase.
+    name = models.CharField(max_length=255)
+    abbreviation = models.CharField(max_length=45, blank=True)
+    class Meta:
+        managed = False
+        db_table = 'courseCoordination'
+
+class Cycle(models.Model):
+    idcycle = models.IntegerField(db_column='idCycle', primary_key=True) # Field name made lowercase.
+    name = models.CharField(max_length=255)
+    abbreviation = models.CharField(max_length=255)
+    idcycletype = models.ForeignKey('Minitablecycletype', db_column='idCycleType') # Field name made lowercase.
+    vacancynumber = models.IntegerField(db_column='vacancyNumber', blank=True, null=True) # Field name made lowercase.
+    startdate = models.DateField(db_column='startDate') # Field name made lowercase.
+    enddate = models.DateField(db_column='endDate') # Field name made lowercase.
+    cyclecode = models.IntegerField(db_column='cycleCode') # Field name made lowercase.
+    dayperiod = models.CharField(db_column='dayPeriod', max_length=45) # Field name made lowercase.
+    termlength = models.ForeignKey('Minitablelength', db_column='termLength') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'cycle'
+
+class Dataanalysisreport(models.Model):
+    iddataanalysisreport = models.IntegerField(db_column='idDataAnalysisReport') # Field name made lowercase.
+    idoffer = models.ForeignKey(AggrOffer, db_column='idOffer') # Field name made lowercase.
+    idclassrepresentative = models.ForeignKey(Classrepresentative, db_column='idClassRepresentative') # Field name made lowercase.
+    dataanalysisreport = models.TextField(db_column='dataAnalysisReport') # Field name made lowercase.
+    submissiontime = models.DateTimeField(db_column='submissionTime', blank=True, null=True) # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'dataAnalysisReport'
+
+class Datafile(models.Model):
+    iddatafile = models.IntegerField(db_column='idDatafile', primary_key=True) # Field name made lowercase.
+    filename = models.CharField(db_column='fileName', max_length=255) # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'datafile'
+
+class Department(models.Model):
+    iddepartment = models.IntegerField(db_column='idDepartment', primary_key=True) # Field name made lowercase.
+    name = models.CharField(max_length=100)
+    departmentcode = models.CharField(db_column='departmentCode', max_length=3) # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'department'
+
+class Encoding(models.Model):
+    idopticalsheet = models.ForeignKey('Opticalsheet', db_column='idOpticalSheet', primary_key=True) # Field name made lowercase.
+    name = models.CharField(max_length=255)
+    class Meta:
+        managed = False
+        db_table = 'encoding'
+
+class Faculty(models.Model):
+    idfaculty = models.IntegerField(db_column='idFaculty', primary_key=True) # Field name made lowercase.
+    name = models.CharField(max_length=255)
+    abbreviation = models.CharField(max_length=45)
+    campus = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=255, blank=True)
+    class Meta:
+        managed = False
+        db_table = 'faculty'
+
+class Minitablecycletype(models.Model):
+    idcycletype = models.IntegerField(db_column='idCycleType', primary_key=True) # Field name made lowercase.
+    name = models.CharField(max_length=255)
+    class Meta:
+        managed = False
+        db_table = 'minitableCycleType'
+
+class Minitabledayoftheweek(models.Model):
+    iddayoftheweek = models.IntegerField(db_column='idDayOfTheWeek', primary_key=True) # Field name made lowercase.
+    dayoftheweek = models.CharField(db_column='dayOfTheWeek', max_length=15) # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'minitableDayOfTheWeek'
+
+class Minitablelength(models.Model):
+    idlength = models.IntegerField(db_column='idLength', primary_key=True) # Field name made lowercase.
+    length = models.CharField(max_length=15)
+    class Meta:
+        managed = False
+        db_table = 'minitableLength'
+
+class Minitablerequirementtype(models.Model):
+    idrequirementtype = models.IntegerField(db_column='idRequirementType', primary_key=True) # Field name made lowercase.
+    name = models.CharField(max_length=45)
+    class Meta:
+        managed = False
+        db_table = 'minitableRequirementType'
+
+class Minitablerequisitiontype(models.Model):
+    idrequisitiontype = models.IntegerField(db_column='idRequisitionType', primary_key=True) # Field name made lowercase.
+    requisitiontype = models.CharField(db_column='requisitionType', max_length=45) # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'minitableRequisitionType'
+
+class Minitablesurveytype(models.Model):
+    idsurveytype = models.IntegerField(db_column='idSurveyType', primary_key=True) # Field name made lowercase.
+    typename = models.CharField(db_column='typeName', max_length=45) # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'minitableSurveyType'
+
+class Opticalsheet(models.Model):
+    idopticalsheet = models.IntegerField(db_column='idOpticalSheet', primary_key=True) # Field name made lowercase.
+    idsurveytype = models.ForeignKey(Minitablesurveytype, db_column='idSurveyType') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'opticalSheet'
+
+class Professor(models.Model):
+    idprofessor = models.AutoField(db_column='idProfessor', primary_key=True) # Field name made lowercase.
+    memberid = models.IntegerField(db_column='memberId') # Field name made lowercase.
+    name = models.CharField(max_length=255)
+    office = models.CharField(max_length=45, null=True, blank=True)
+    email = models.CharField(max_length=65, null=True, blank=True)
+    phonenumber = models.IntegerField(db_column='phoneNumber', blank=True, null=True) # Field name made lowercase.
+    cellphonenumber = models.IntegerField(db_column='cellphoneNumber', blank=True, null=True) # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'professor'
+
+class Question(models.Model):
+    idquestion = models.IntegerField(db_column='idQuestion') # Field name made lowercase.
+    idanswertype = models.ForeignKey(Answertype, db_column='idAnswerType') # Field name made lowercase.
+    questionwording = models.TextField(db_column='questionWording') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'question'
+
+class Questionnaire(models.Model):
+    idquestionnaire = models.IntegerField(db_column='idQuestionnaire', primary_key=True) # Field name made lowercase.
+    description = models.CharField(max_length=255)
+    creationdate = models.DateField(db_column='creationDate') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'questionnaire'
+
+class RelAcademicprogramCycle(models.Model):
+    idacademicprogram = models.ForeignKey(Academicprogram, db_column='idAcademicProgram') # Field name made lowercase.
+    idcycle = models.ForeignKey(Cycle, db_column='idCycle') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'rel_academicProgram_cycle'
+
+class RelAnswerOpticalsheetfieldSurvey(models.Model):
+    idanswer = models.ForeignKey(Answer, db_column='idAnswer') # Field name made lowercase.
+    idopticalsheetfield = models.ForeignKey(AggrOpticalsheetfield, db_column='idOpticalSheetField') # Field name made lowercase.
+    idsurvey = models.ForeignKey(AggrSurvey, db_column='idSurvey') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'rel_answer_opticalSheetField_survey'
+
+class RelClassrepresentativeOpticalsheet(models.Model):
+    idclassrepresentative = models.ForeignKey(Classrepresentative, db_column='idClassRepresentative') # Field name made lowercase.
+    idopticalsheet = models.ForeignKey(Opticalsheet, db_column='idOpticalSheet') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'rel_classRepresentative_opticalSheet'
+
+class RelCoursecoordinationCycle(models.Model):
+    idcoursecoordination = models.ForeignKey(Coursecoordination, db_column='idCourseCoordination') # Field name made lowercase.
+    idcycle = models.ForeignKey(Cycle, db_column='idCycle') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'rel_courseCoordination_cycle'
+
+class RelCoursecoordinationFaculty(models.Model):
+    idcoursecoordination = models.ForeignKey(Coursecoordination, db_column='idCourseCoordination') # Field name made lowercase.
+    idfaculty = models.ForeignKey(Faculty, db_column='idFaculty') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'rel_courseCoordination_faculty'
+
+class RelCourseCycle(models.Model):
+    idcourse = models.ForeignKey(Course, db_column='idCourse') # Field name made lowercase.
+    idcycle = models.ForeignKey(Cycle, db_column='idCycle') # Field name made lowercase.
+    term = models.IntegerField()
+    startdate = models.DateField(db_column='startDate') # Field name made lowercase.
+    enddate = models.DateField(db_column='endDate') # Field name made lowercase.
+    requisitiontype = models.ForeignKey(Minitablerequisitiontype, db_column='requisitionType', blank=True, null=True) # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'rel_course_cycle'
+
+class RelCourseCycleCourse(models.Model):
+    idcycle = models.ForeignKey(Cycle, db_column='idCycle') # Field name made lowercase.
+    idcourse = models.ForeignKey(Course, db_column='idCourse', related_name='idCourse_relCourseCycleCourse') # Field name made lowercase.
+    idrequirement = models.ForeignKey(Course, db_column='idRequirement', related_name='idRequirement_relCourseCycleCourse') # Field name made lowercase.
+    idrequirementtype = models.ForeignKey(Minitablerequirementtype, db_column='idRequirementType') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'rel_course_cycle_course'
+
+class RelCycleOpticalsheet(models.Model):
+    idopticalsheet = models.ForeignKey(Opticalsheet, db_column='idOpticalSheet') # Field name made lowercase.
+    idcycle = models.ForeignKey(Cycle, db_column='idCycle') # Field name made lowercase.
+    term = models.IntegerField()
+    class Meta:
+        managed = False
+        db_table = 'rel_cycle_opticalSheet'
+
+class RelDepartmentProfessor(models.Model):
+    idprofessor = models.ForeignKey(Professor, db_column='idProfessor') # Field name made lowercase.
+    iddepartment = models.ForeignKey(Department, db_column='idDepartment') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'rel_department_professor'
+
+class RelOfferSchedule(models.Model):
+    idschedule = models.ForeignKey('Schedule', db_column='idSchedule') # Field name made lowercase.
+    idoffer = models.ForeignKey(AggrOffer, db_column='idOffer') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'rel_offer_schedule'
+
+class RelQuestionQuestionnaire(models.Model):
+    idquestionnaire = models.ForeignKey(Questionnaire, db_column='idQuestionnaire') # Field name made lowercase.
+    idquestion = models.ForeignKey(Question, db_column='idQuestion') # Field name made lowercase.
+    questionindex = models.IntegerField(db_column='questionIndex') # Field name made lowercase.
+    class Meta:
+        managed = False
+        db_table = 'rel_question_questionnaire'
+
+class Schedule(models.Model):
+    idschedule = models.IntegerField(db_column='idSchedule', primary_key=True) # Field name made lowercase.
+    iddayoftheweek = models.ForeignKey(Minitabledayoftheweek, db_column='idDayOfTheWeek') # Field name made lowercase.
+    start = models.TimeField()
+    end = models.TimeField()
+    frequency = models.CharField(max_length=45)
+    class Meta:
+        managed = False
+        db_table = 'schedule'
+
+class Timeperiod(models.Model):
+    idtimeperiod = models.IntegerField(db_column='idTimePeriod', primary_key=True) # Field name made lowercase.
+    length = models.ForeignKey(Minitablelength, db_column='length')
+    year = models.IntegerField()
+    session = models.IntegerField()
+    class Meta:
+        managed = False
+        db_table = 'timePeriod'
+
--- interface/tests.py
+++ interface/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
 from django.test.testcases import LiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
+from pulsarInterface.Professor import Professor
 
 def open_page(browser, url, live_server_url):
     browser.get('%s%s' % (live_server_url,url))
@@ -21,11 +21,34 @@ class ProfessorTest(LiveServerTestCase):
     
     def setUp(self):
         self.browser = WebDriver()
-        self.browser.implicitly_wait(5)
+        self.browser.implicitly_wait(1)
         create_user_and_login(self.browser, self.live_server_url,'john','johnpassword','john@john.com')
+        self.name_professor = 'teste'
 
     def tearDown(self):
         self.browser.quit()
+        
+    def create_professor(self,professor_name):
+        open_page(self.browser, '/interface/professor', self.live_server_url)
+        button_create_professor = self.browser.find_element_by_name('criar')
+        button_create_professor.click()
+        self.assertIn('Interface - Professor Create', self.browser.title)
+        form_name = self.browser.find_element_by_id('id_name')
+        form_name.send_keys(professor_name)
+        form_memberId = self.browser.find_element_by_id('id_memberId')
+        form_memberId.send_keys('00000')
+        form_office = self.browser.find_element_by_id('id_office')
+        form_office.send_keys('0')
+        form_email = self.browser.find_element_by_id('id_email')
+        form_email.send_keys('0')
+        form_phoneNumber = self.browser.find_element_by_id('id_phoneNumber')
+        form_phoneNumber.send_keys('0')
+        form_cellphoneNumber = self.browser.find_element_by_id('id_cellphoneNumber')
+        form_cellphoneNumber.send_keys('0')
+        form_idDepartment = self.browser.find_element_by_id('id_idDepartment')
+        form_idDepartment.send_keys('')
+        button_submit = self.browser.find_element_by_name('Cadastrar')
+        button_submit.click()
 
     def test_login_to_interface_page(self):
         self.assertIn('Index', self.browser.title)
@@ -40,27 +63,60 @@ class ProfessorTest(LiveServerTestCase):
         self.assertIn('Interface - Professor', self.browser.title)
         
     def test_create_professor(self):
+        self.create_professor(self.name_professor)
         open_page(self.browser, '/interface/professor', self.live_server_url)
-        button_create_professor = self.browser.find_element_by_name('criar')
-        button_create_professor.click()
-        self.assertIn('Interface - Professor Create', self.browser.title)
+        professor_name_link = self.browser.find_element_by_link_text(self.name_professor)
+        self.assertIsNotNone(professor_name_link)
+        
+    def test_edit_professor(self):
+        professor = Professor(self.name_professor)
+        professor.store()
+        open_page(self.browser, '/interface/professor', self.live_server_url)
+        professor_name_link = self.browser.find_element_by_link_text(self.name_professor)
+        professor_name_link.click()
+        self.assertIn('Interface - Professor Detail', self.browser.title)
+        button_edit = self.browser.find_element_by_name('editar')
+        button_edit.click()
+        self.assertIn('Interface - Professor Edit', self.browser.title)
         form_name = self.browser.find_element_by_id('id_name')
-        form_name.send_keys('teste')
+        form_name.send_keys('Edit')
         form_memberId = self.browser.find_element_by_id('id_memberId')
-        form_memberId.send_keys('00000')
+        form_memberId.send_keys('')
         form_office = self.browser.find_element_by_id('id_office')
-        form_office.send_keys('0')
+        form_office.send_keys('')
         form_email = self.browser.find_element_by_id('id_email')
-        form_email.send_keys('0')
+        form_email.send_keys('')
         form_phoneNumber = self.browser.find_element_by_id('id_phoneNumber')
         form_phoneNumber.send_keys('0')
         form_cellphoneNumber = self.browser.find_element_by_id('id_cellphoneNumber')
         form_cellphoneNumber.send_keys('0')
         form_idDepartment = self.browser.find_element_by_id('id_idDepartment')
         form_idDepartment.send_keys('')
-        button_apply = self.browser.find_element_by_name('aplicar')
+        button_apply = self.browser.find_element_by_name('Aplicar')
         button_apply.click()
         open_page(self.browser, '/interface/professor', self.live_server_url)
-        professor_name = self.browser.find_element_by_link_text('teste')
-        self.assertIsNotNone(professor_name)
-        
+        professor_name_link_after_edit = self.browser.find_element_by_link_text(self.name_professor + 'Edit')
+        professor_name_link_after_edit.click()
+        list_professor_info = self.browser.find_elements_by_tag_name('p')
+        self.assertEqual(list_professor_info[1].text, 'Member ID: 0')
+        self.assertEqual(list_professor_info[2].text, 'Office: None')
+        self.assertEqual(list_professor_info[3].text, 'Email: None')
+        self.assertEqual(list_professor_info[4].text, 'Phone Number: 0')
+        self.assertEqual(list_professor_info[5].text, 'CellPhone Number: 0')
+        self.assertEqual(list_professor_info[6].text, 'Id Department: None')
+    
+    def test_delete_professor(self):
+        professor = Professor(self.name_professor)
+        professor.store()
+        open_page(self.browser, '/interface/professor', self.live_server_url)
+        professor_name_link = self.browser.find_element_by_link_text(self.name_professor)
+        professor_name_link.click()
+        self.assertIn('Interface - Professor Detail', self.browser.title)
+        button_delete = self.browser.find_element_by_name('deletar')
+        button_delete.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
+        self.assertIn('Interface - Professor', self.browser.title)
+        professor_name_after_delete = self.browser.find_elements_by_tag_name('a')
+        names = [link.text for link in professor_name_after_delete]
+        self.assertNotIn(self.name_professor, names)
